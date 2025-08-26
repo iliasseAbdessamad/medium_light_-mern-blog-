c8007aadfc792c90dc767ddef2e41090a6251b55
iliasseAbdessamad <samwillh7@gmail.com>
2025-08-26 05:55:46 +0100

    Add Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,18 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+
+import Navbar from "./components/navbar/Navbar"
 import './App.css'
 
 
-library.add(fas)
+
 
 
 function App() {
   
 
   return (
-    <h1>
-      App
-    </h1>
+    <>
+      <Navbar />
+    </>
   )
 }
 
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react"
+import logo from "../../assets/logo.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { Link } from "react-router"
+import './navbar.css'
+
+library.add(fas)
+
+
+function Navbar() {
+
+    const [searchVisible, setSearchVisible] = useState(window.innerWidth > 800);
+
+    const handleSearchVisibility = (e) => {
+        setSearchVisible(prevState => !prevState)
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", (e) => {
+            if(window.innerWidth > 800){
+                setSearchVisible(true)
+            }
+        });
+    }, [])
+
+    return (
+        <nav className="navbar">
+            <div className="navbar_left">
+                <Link to="/">
+                    <img src={logo} className="navbar_logo" alt="website logo" />
+                </Link>
+                <div 
+                    className="navbar_search-wrapper" 
+                    style={{display:searchVisible ? "block" : "none"}}
+                >
+                    <div className="navbar_search">
+                        <input 
+                            type="text"
+                            placeholder="Search..."
+                            className="navbar_search-input"
+                        />
+                        <FontAwesomeIcon icon="fa-solid fa-search" className="navbar_search-icon" />
+                    </div>
+                </div>
+            </div>
+            <div className="navbar_right">
+                <div className="navbar_right-search" onClick={handleSearchVisibility}>
+                    <FontAwesomeIcon icon="fa-solid fa-search" />
+                </div>
+                <Link to="/write" className="navbar_btn-write">
+                    <FontAwesomeIcon icon="fa-solid fa-edit" />
+                </Link>
+                <Link to="/signin" className="navbar_btn-signin">
+                    Sign In
+                </Link>
+                <Link to="/signup" className="navbar_btn-signup">
+                    Sign Up
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
