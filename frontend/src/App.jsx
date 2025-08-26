09a46af829e7cef37da3919d45d7bd8f4efa1559
iliasseAbdessamad <samwillh7@gmail.com>
2025-08-26 02:33:19 +0100

    First commit (Basic setup)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import './App.css'
+
+
+library.add(fas)
+
+
+function App() {
+  
+
+  return (
+    <h1>
+      App
+    </h1>
+  )
+}
+
+export default App
